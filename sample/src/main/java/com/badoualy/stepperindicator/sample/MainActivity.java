4fbadcc86c78ed2a35370bb49b7d6261091e8141
Yann Badoual <yann.badoual@gmail.com>
2017-07-12 20:21:48 +0900

    Fix some bugs with label (missing requestLayout call) Add possibility to use custom done icon

--- sample/src/main/java/com/badoualy/stepperindicator/sample/MainActivity.java
+++ sample/src/main/java/com/badoualy/stepperindicator/sample/MainActivity.java
@@ -2,15 +2,8 @@ package com.badoualy.stepperindicator.sample;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.badoualy.stepperindicator.StepperIndicator;
 
@@ -21,12 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        final ViewPager pager = findViewById(R.id.pager);
         assert pager != null;
-        pager.setAdapter(new EmptyPagerAdapter(getSupportFragmentManager()));
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
 
-        StepperIndicator indicator = (StepperIndicator) findViewById(R.id.stepper_indicator);
-        assert indicator != null;
+        final StepperIndicator indicator = findViewById(R.id.stepper_indicator);
         // We keep last page for a "finishing" page
         indicator.setViewPager(pager, true);
 
@@ -38,58 +30,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public static class PageFragment extends Fragment {
-
-        private TextView lblPage;
-
-        public static PageFragment newInstance(int page, boolean isLast) {
-            Bundle args = new Bundle();
-            args.putInt("page", page);
-            if (isLast)
-                args.putBoolean("isLast", true);
-            final PageFragment fragment = new PageFragment();
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            final View view = inflater.inflate(R.layout.fragment_page, container, false);
-            lblPage = (TextView) view.findViewById(R.id.lbl_page);
-            return view;
-        }
-
-        @Override
-        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            final int page = getArguments().getInt("page", 0);
-            if (getArguments().containsKey("isLast"))
-                lblPage.setText("You're done!");
-            else
-                lblPage.setText(Integer.toString(page));
-        }
-    }
-
-    private static class EmptyPagerAdapter extends FragmentPagerAdapter {
-
-        public EmptyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public int getCount() {
-            return 5;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return PageFragment.newInstance(position + 1, position == getCount() - 1);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Page " + position;
-        }
-    }
 }
